08e7f44b80f5ce0d7edc8eff37620786631e98aa
Alexander Samaniego <69481177+alexsam29@users.noreply.github.com>
2023-06-07 14:01:57 -0400

    Add createFragment api call

--- src/api.js
+++ src/api.js
@@ -1,4 +1,3 @@
-// fragments microservice API
 const apiUrl = process.env.API_URL || 'http://localhost:8080';
 
 // Given an authenticated user, request all fragments for this user
@@ -16,5 +15,28 @@ export async function getUserFragments(user) {
     console.log('Got user fragments data', { data });
   } catch (err) {
     console.error('Unable to call GET /v1/fragment', { err });
+  }
+}
+
+// Given an authenticated user and text, create a new fragment
+export async function createFragment(user, fragmentText) {
+  console.log('Creating a new fragment...');
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      method: 'POST',
+      headers: {
+        ...user.authorizationHeaders('text/plain'),
+      },
+      body: fragmentText,
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.text();
+    return JSON.parse(data);
+    
+  } catch (err) {
+    console.error('Unable to create a new fragment', { err });
   }
 }
